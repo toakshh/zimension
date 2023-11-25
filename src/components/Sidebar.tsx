fbb50ac753d56267b3b8fd8267926db45781251a
Aksh <toakshh@gmail.com>
2023-11-25 09:25:01 +0530

    sidebar ui updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <aside className="w-[20%] bg-gray-300">Sidebar</aside>;
+  return (
+    <aside className="lg:w-[20%] w-full bg-gray-300 flex flex-col justify-center items-center border-2 border-red-600 p-5 gap-5">
+      <h3 className="text-lg font-light">Add New Operation </h3>
+      <form className="flex flex-col lg:w-full sm:w-[50%] w-full gap-4">
+        <input
+          type="text"
+          placeholder="Enter name"
+          className="px-2 py-2 rounded-xl border border-gray-900"
+        />
+        <select>
+          {Array(5)
+            .fill(0)
+            .map((e, i) => (
+              <option key={e + i} value="i+1">
+                {i + 1}
+              </option>
+            ))}
+        </select>
+        <button className="btn-danger rounded-md hover:hover-cards shadow-2xl">
+          Add
+        </button>
+      </form>
+    </aside>
+  );
 };
 
 export default Sidebar;
